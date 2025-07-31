25d7d10e3bb94b7f0c055cf8001649d42c3dcf6d
ARYAN x SHOAIB <shoaib910385@gmail.com>
2025-07-31 11:29:42 +0530

    Update start.py

--- modules/start.py
+++ modules/start.py
@@ -4,7 +4,7 @@ from pyrogram import Client, filters, types as t
 from bot import StartTime
 
 startText = """
-An AntiNSFW bot Powered by @LexicaAPI to protect your groups from NSFW content.
+An AntiNSFW bot Powered by @thedrxnet to protect your groups from NSFW content.
 """
 
 @Client.on_message(filters.command(["start","help","repo","source"]))
@@ -14,7 +14,7 @@ async def start(_: Client, m: t.Message):
         reply_markup=t.InlineKeyboardMarkup(
             [
                 [
-                    t.InlineKeyboardButton(text="Source",url="https://github.com/Qewertyy/AntiNSFWRobot")
+                    t.InlineKeyboardButton(text="˹sᴜᴘᴘᴏʀᴛ˼",url="https://t.me/Hehe_Stalker")
                 ]
             ]
         )
